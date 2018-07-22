7f1f2d6810ff75b9eb1e018d6ab7fec0aaa349a3
Frank W Miller <frank@frankwmiller.net>
2018-07-22 21:13:59 +0000

    Added float.h macros

--- include/float.h
+++ include/float.h
@@ -1,4 +1,21 @@
 #ifndef __FLOAT_H
 #define __FLOAT_H
 
+#define FLT_RADIX
+#define FLT_ROUNDS
+#define FLT_DIG
+#define FLT_EPSILON
+#define FLT_MANT_DIG
+#define FLT_MAX
+#define FLT_MAX_EXP
+#define FLT_MIN
+#define FLT_MIN_EXP
+#define DBL_DIG
+#define DBL_EPSILON
+#define DBL_MANT_DIG
+#define DBL_MAX
+#define DBL_MAX_EXP
+#define DBL_MIN
+#define DBL_MIN_EXP
+
 #endif
