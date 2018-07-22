48fc8a5e4cff3ebe0f7b9833dd99fb5fb257f644
Frank W Miller <frank@frankwmiller.net>
2018-07-22 21:11:17 +0000

    Added limits.h macros

--- include/limits.h
+++ include/limits.h
@@ -1,4 +1,20 @@
 #ifndef __LIMITS_H
 #define __LIMITS_H
 
+#define CHAR_BIT
+#define CHAR_MAX
+#define CHAR_MIN
+#define INT_MAX
+#define INT_MIN
+#define LONG_MAX
+#define LONG_MIN
+#define SCHAR_MAX
+#define SCHAR_MIN
+#define SHRT_MAX
+#define SHRT_MIN
+#define UCHAR_MAX
+#define UINT_MAX
+#define ULONG_MAX
+#define USHRT_MAX
+
 #endif
