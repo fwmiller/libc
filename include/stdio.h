7bf719c06578573adeea0478b976d1b0bd28c8a8
Frank W Miller <frank@frankwmiller.net>
2018-07-22 20:14:25 +0000

    Added stdio.h functions

--- include/stdio.h
+++ include/stdio.h
@@ -1,4 +1,59 @@
 #ifndef __STDIO_H
 #define __STDIO_H
 
+/* File Operations */
+FILE *fopen(const char *filename, const char *mode);
+FILE *freopen(const char *filename, const char *mode, FILE *stream);
+int fflush(FILE *stream);
+int fclose(FILE *stream);
+int remove(const char *filename);
+int rename(const char *oldname, const char *newname);
+FILE *tmpfile(void);
+char *tmpnam(char s[L_tmpnam]);
+int setvbuf(FILE *stream, char *buf, int mode, size_t size);
+void setbuf(FILE *stream, char *buf);
+
+/* Formatted Output */
+int fprintf(FILE *stream, const char *format, ...);
+int printf(const char *format, ...);
+int sprintf(char *s, const char *format, ...);
+int vprintf(const char *format, va_list arg);
+int vfprintf(FILE *stream, const char *format, va_list arg);
+int vsprintf(char *s, const char *format, va_list arg);
+
+/* Formatted Input */
+int fscanf(FILE *stream, const char *format, ...);
+int scanf(const char *format, ...);
+int sscanf(char *s, const char *format, ...);
+
+/* Character Input and Output Functions */
+int fgetc(FILE *stream);
+char *fgets(char *s, int n, FILE *stream);
+int fputc(int c, FILE *stream);
+int fputs(const char *s, FILE *stream);
+int getc(FILE *stream);
+int getchar(void);
+char *gets(char *s);
+int putc(int c, FILE *stream);
+int putchar(int c);
+int puts(const char *s);
+int ungetc(int c, FILE *stream);
+
+/* Direct Input and Output Functions */
+size_t fread(void *ptr, size_t size, size_t nobj, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t nobj, FILE *stream);
+
+/* File Positioning Functions */
+int fseek(FILE *stream, long offset, int origin);
+long ftell(FILE *stream);
+void rewind(FILE *stream);
+int fgetpos(FILE *stream, fpos_t *ptr);
+int fsetpos(FILE *stream, const fpos_t *ptr);
+
+/* Error Functions */
+void clearerr(PILE *stream);
+int feof(FILE *stream);
+int ferror(FILE *stream);
+void perror(const char *s);
+
 #endif
